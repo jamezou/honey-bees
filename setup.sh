28981517749af23a9e8736d24c08b59ddd61ff6f
jamez <jamezou18@gmail.com>
2022-04-28 15:35:08 -0400

    setup update

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"zou_jame@bentley.edu\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
